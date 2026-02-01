d122fab4c33be7cf0ebf271ae8e6944bf409bbc5
Lang Yu Xin <lang.xin+f8u@montel.energy>
2026-01-31 19:09:50 -0700

    Add ComboStep struct and implementation for nested test steps in TestBot

--- src/testbot/mod.rs
+++ src/testbot/mod.rs
@@ -3,6 +3,7 @@ pub mod bot;
 pub mod context;
 pub mod error;
 pub mod http;
+pub mod combo;
 pub mod scenario;
 pub mod step;
 
@@ -11,6 +12,7 @@ pub use bot::TestBot;
 pub use context::TestContext;
 pub use error::TestError;
 pub use http::{HttpClient, HttpResponse};
+pub use combo::ComboStep;
 pub use scenario::TestScenario;
 pub use step::TestStep;
 
