4706cf7670ccd8a0ed0f4bb0c050f825f1ee2a52
Lang Yu Xin <lang.xin+f8u@montel.energy>
2026-07-11 22:50:30 -0600

    feat: update sqlite_default function to use randomblob for UUID generation

--- src/data/sqlite/migration.rs
+++ src/data/sqlite/migration.rs
@@ -141,7 +141,7 @@ impl MigrationBuilder {
 
     pub fn sqlite_default(default: &str) -> String {
         match default.trim().to_ascii_lowercase().as_str() {
-        "gen_random_uuid()" => "(lower(hex(randomblob(4))) || '-' || lower(hex(randomblob(2))) || '-4' || substr(lower(hex(randomblob(2))),2) || '-' || substr('89ab',abs(random()) % 4 + 1,1) || substr(lower(hex(randomblob(2))),2) || '-' || lower(hex(randomblob(6))))".into(),
+        "gen_random_uuid()" => "(randomblob(16))".into(),
         "now()" => "CURRENT_TIMESTAMP".into(),
         _ => default.to_string(),
     }
